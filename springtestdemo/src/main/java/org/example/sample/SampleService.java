6a79d8c835b65cbead940a0e8e7cc4382f17ffd2
jiseung-gu <tmdrn457@naver.com>
2021-04-29 09:30:16 +0900

    springtestdemo

---
+++ springtestdemo/src/main/java/org/example/sample/SampleService.java
@@ -0,0 +1,14 @@
+package org.example.sample;
+
+
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SampleService {
+
+
+    public String getName() {
+        return "hello222";
+    }
+}
---
+++ springtestdemo/src/test/java/org/example/sample/SampleControllerTest.java
@@ -0,0 +1,42 @@
+package org.example.sample;
+
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(SampleController.class)
+public class  SampleControllerTest {
+
+    @MockBean
+    SampleService mockSampleService;
+
+    @Autowired
+    MockMvc mockMvc;
+
+
+    @Test
+    public void hello() throws Exception {
+        when(mockSampleService.getName()).thenReturn("SeungGu");
+
+        mockMvc.perform(get("/hello"))
+                .andExpect(content().string("hello SeungGu"));
+
+    }
+}
