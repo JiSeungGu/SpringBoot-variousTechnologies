2f0090536b37d014003b9333d5044616ff4891c7
jiseung-gu <tmdrn457@naver.com>
2021-05-12 10:31:02 +0900

    SpringSecurity

--- springbootsecurity/src/main/java/com/example/springbootsecurity/WebConfig.java
+++ springbootsecurity/src/main/java/com/example/springbootsecurity/WebConfig.java
@@ -1,2 +1,21 @@
-package com.example.springbootsecurity;public class WebConfig {
+package com.example.springbootsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+
+    // Controller에서 할 로직이 없고 단순 View로만 보내면 될때
+    // 특정요청이 올때 특정뷰를 보내면 될때 ViewController를 사용하면 된다.
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/hello").setViewName("hello");
+
+    }
 }
